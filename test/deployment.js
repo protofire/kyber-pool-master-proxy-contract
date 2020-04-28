323ee5d82631e2f28947d356e777f273ce04954e
Lisandro Corbalan <lmcorbalan@gmail.com>
2020-04-28 09:45:29 -0300

    Test

--- test/deployment.js
+++ test/deployment.js
@@ -3,54 +3,60 @@ const KyberPoolMaster = artifacts.require('KyberPoolMaster');
 const {expect} = require('chai');
 const {expectRevert} = require('@openzeppelin/test-helpers');
 
+const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
+const NO_ZERO_ADDRESS = '0x0000000000000000000000000000000000000001';
+
 contract('KyberPoolMaster test', async (accounts) => {
   describe('deplyment', () => {
     it('should not allow to deploy a KyberPoolMaster zero address kncToken', async () => {
       await expectRevert(
         KyberPoolMaster.new(
-          '0x0000000000000000000000000000000000000000',
-          '0x0000000000000000000000000000000000000000',
-          '0x0000000000000000000000000000000000000000',
-          '0x0000000000000000000000000000000000000000',
+          ZERO_ADDRESS,
+          ZERO_ADDRESS,
+          ZERO_ADDRESS,
+          ZERO_ADDRESS,
           0,
           0
         ),
         'ctor: kncToken is missing'
       );
     });
+
     it('should not allow to deploy a KyberPoolMaster zero address kyberDAO', async () => {
       await expectRevert(
         KyberPoolMaster.new(
-          '0x0000000000000000000000000000000000000001',
-          '0x0000000000000000000000000000000000000000',
-          '0x0000000000000000000000000000000000000000',
-          '0x0000000000000000000000000000000000000000',
+          NO_ZERO_ADDRESS,
+          ZERO_ADDRESS,
+          ZERO_ADDRESS,
+          ZERO_ADDRESS,
           0,
           0
         ),
         'ctor: kyberDAO is missing'
       );
     });
+
     it('should not allow to deploy a KyberPoolMaster zero address kyberStaking', async () => {
       await expectRevert(
         KyberPoolMaster.new(
-          '0x0000000000000000000000000000000000000001',
-          '0x0000000000000000000000000000000000000001',
-          '0x0000000000000000000000000000000000000000',
-          '0x0000000000000000000000000000000000000000',
+          NO_ZERO_ADDRESS,
+          NO_ZERO_ADDRESS,
+          ZERO_ADDRESS,
+          ZERO_ADDRESS,
           0,
           0
         ),
         'ctor: kyberStaking is missing'
       );
     });
+
     it('should not allow to deploy a KyberPoolMaster zero address kyberFeeHandler', async () => {
       await expectRevert(
         KyberPoolMaster.new(
-          '0x0000000000000000000000000000000000000001',
-          '0x0000000000000000000000000000000000000001',
-          '0x0000000000000000000000000000000000000001',
-          '0x0000000000000000000000000000000000000000',
+          NO_ZERO_ADDRESS,
+          NO_ZERO_ADDRESS,
+          NO_ZERO_ADDRESS,
+          ZERO_ADDRESS,
           0,
           0
         ),
@@ -61,10 +67,10 @@ contract('KyberPoolMaster test', async (accounts) => {
     it('should not allow to deploy a KyberPoolMaster with epochNotice lower than the minimum', async () => {
       await expectRevert(
         KyberPoolMaster.new(
-          '0x0000000000000000000000000000000000000001',
-          '0x0000000000000000000000000000000000000001',
-          '0x0000000000000000000000000000000000000001',
-          '0x0000000000000000000000000000000000000001',
+          NO_ZERO_ADDRESS,
+          NO_ZERO_ADDRESS,
+          NO_ZERO_ADDRESS,
+          NO_ZERO_ADDRESS,
           0,
           0
         ),
@@ -74,29 +80,25 @@ contract('KyberPoolMaster test', async (accounts) => {
 
     it('should set the right parameters', async () => {
       const poolMater = await KyberPoolMaster.new(
-        '0x0000000000000000000000000000000000000001',
-        '0x0000000000000000000000000000000000000001',
-        '0x0000000000000000000000000000000000000001',
-        '0x0000000000000000000000000000000000000001',
+        NO_ZERO_ADDRESS,
+        NO_ZERO_ADDRESS,
+        NO_ZERO_ADDRESS,
+        NO_ZERO_ADDRESS,
         2,
         1
       );
 
       const kncToken = await poolMater.kncToken();
-      expect(kncToken).to.equal('0x0000000000000000000000000000000000000001');
+      expect(kncToken).to.equal(NO_ZERO_ADDRESS);
 
       const kyberDAO = await poolMater.kyberDAO();
-      expect(kyberDAO).to.equal('0x0000000000000000000000000000000000000001');
+      expect(kyberDAO).to.equal(NO_ZERO_ADDRESS);
 
       const kyberStaking = await poolMater.kyberStaking();
-      expect(kyberStaking).to.equal(
-        '0x0000000000000000000000000000000000000001'
-      );
+      expect(kyberStaking).to.equal(NO_ZERO_ADDRESS);
 
       const kyberFeeHandler = await poolMater.kyberFeeHandler();
-      expect(kyberFeeHandler).to.equal(
-        '0x0000000000000000000000000000000000000001'
-      );
+      expect(kyberFeeHandler).to.equal(NO_ZERO_ADDRESS);
 
       const epochNotice = await poolMater.epochNotice();
       expect(epochNotice.toString()).to.equal('2');
