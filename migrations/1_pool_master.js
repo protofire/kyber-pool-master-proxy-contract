774273b61ae2a3b409aebb428a6867cda2470434
Lisandro Corbalan <lmcorbalan@gmail.com>
2020-06-16 11:54:49 -0300

    Update migration script for using new env variable on KyberPoolMaster deployment

--- migrations/1_pool_master.js
+++ migrations/1_pool_master.js
@@ -1,6 +1,7 @@
 const KyberPoolMaster = artifacts.require('KyberPoolMaster');
 
 const KYBER_DAO_ADDRESS = process.env.KYBER_DAO_ADDRESS;
+const KYBER_FEE_HANDLER_ADDRESS = process.env.KYBER_FEE_HANDLER_ADDRESS;
 const EPOCH_NOTICE = process.env.EPOCH_NOTICE;
 const INITIAL_DELEGATION_FEE = process.env.INITIAL_DELEGATION_FEE;
 
@@ -9,6 +10,7 @@ module.exports = async function (deployer) {
     await deployer.deploy(
       KyberPoolMaster,
       KYBER_DAO_ADDRESS,
+      KYBER_FEE_HANDLER_ADDRESS,
       EPOCH_NOTICE,
       INITIAL_DELEGATION_FEE
     );
